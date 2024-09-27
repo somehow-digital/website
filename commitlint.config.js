33fdf1987227ff9a68385bd24429e4e3e36124bd
Thomas Deinhamer <code@thasmo.dev>
2024-09-27 14:44:28 +0200

    Use shared commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,3 @@
 export default {
-	plugins: ['@somehow-digital/commitlint-plugin-spellcheck'],
-	rules: {
-		'spellcheck/header': [2, 'always'],
-		'spellcheck/body': [2, 'always'],
-		'spellcheck/footer': [2, 'always'],
-		'spellcheck/scope': [2, 'always'],
-		'spellcheck/subject': [2, 'always'],
-		'spellcheck/type': [2, 'always'],
-	},
+	extends: ['@somehow-digital/commitlint-config'],
 };
