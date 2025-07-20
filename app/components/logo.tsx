abd5cc3f500a7ad55cabdc5a859ef14fa11323cd
Thomas Deinhamer <code@thasmo.dev>
2025-07-20 20:00:23 +0200

    Adapt project for new eslint configuration

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,62 +1,69 @@
 import type { FunctionComponent } from 'react';
-
-import { useEffect, useRef } from 'react';
-
+import { useEffect, useMemo, useRef } from 'react';
 import type { LogoAnimationReferences } from '~/components/logo.animation';
-
 import { animation } from '~/components/logo.animation';
-
 import * as styles from './logo.styles';
 
 export const Logo: FunctionComponent = () => {
-	const references: LogoAnimationReferences = {
-		bars: useRef<HTMLElement[]>([]),
-		base: useRef<HTMLHeadingElement>(null),
-		dot: useRef<HTMLElement>(null),
-		mark: useRef<HTMLElement>(null),
-		type: useRef<HTMLElement[]>([]),
-	};
+	const barsRef = useRef<HTMLElement[]>([]);
+	const baseRef = useRef<HTMLHeadingElement>(null);
+	const dotRef = useRef<HTMLElement>(null);
+	const markRef = useRef<HTMLElement>(null);
+	const typeRef = useRef<HTMLElement[]>([]);
+
+	const references: LogoAnimationReferences = useMemo(() => ({
+		bars: barsRef,
+		base: baseRef,
+		dot: dotRef,
+		mark: markRef,
+		type: typeRef,
+	}), []);
 
 	useEffect(() => {
 		const controls = animation(references);
 		return () => controls?.cancel();
-	}, []);
+	}, [references]);
 
 	return (
-		<h1 className={styles.base} ref={references.base}>
+		<h1 className={styles.base} ref={baseRef}>
 			<span className={styles.type}>
 				<span
 					className={styles.phrase}
 					ref={(element) => {
-						if (element && references.type.current) {
-							references.type.current[0] = element;
+						if (element && typeRef.current) {
+							typeRef.current[0] = element;
 						}
-					}}>
+					}}
+				>
 					somehow
 				</span>
 
 				<span
 					className={styles.phrase}
 					ref={(element) => {
-						if (element && references.type.current) {
-							references.type.current[1] = element;
+						if (element && typeRef.current) {
+							typeRef.current[1] = element;
 						}
-					}}>
-					digital<span ref={references.dot}>.</span>
+					}}
+				>
+					digital
+					<span ref={dotRef}>.</span>
 				</span>
 			</span>
 
-			<span aria-hidden="true" className={styles.mark} ref={references.mark}>
+			<span aria-hidden="true" className={styles.mark} ref={markRef}>
 				{[1, 2, 3, 4, 5].map((value, index) => {
 					return (
 						<span
 							className={styles.bar}
 							key={value}
 							ref={(element) => {
-								if (element && references.bars.current) {
-									references.bars.current[index] = element;
+								if (element && barsRef.current) {
+									barsRef.current[index] = element;
 								}
-							}}></span>
+							}}
+						>
+						</span>
 					);
 				})}
 			</span>
--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,5 +1,4 @@
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
-
 import * as build from '../build/server';
 
 export const onRequest: (context: EventContext<never, never, never>) => Promise<Response> = createPagesFunctionHandler({
