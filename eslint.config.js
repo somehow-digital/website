ea76dfb577f7babfc3f1174b62d0bf938d50cc71
Thomas Deinhamer <code@thasmo.dev>
2025-08-06 21:25:50 +0200

    install shared eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,3 @@
-import antfu from '@antfu/eslint-config';
+import config from '@somehow-digital/eslint-config';
 
-export default antfu({
-	formatters: {
-		css: true,
-		html: true,
-		markdown: true,
-		svg: true,
-		xml: true,
-	},
-	imports: {
-		overrides: {
-			'perfectionist/sort-imports': [
-				'error',
-				{
-					newlinesBetween: 0,
-				},
-			],
-		},
-	},
-	stylistic: {
-		indent: 'tab',
-		semi: true,
-	},
-});
+export default config();
