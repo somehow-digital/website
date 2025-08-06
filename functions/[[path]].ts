a221b33139313cc67de4f7a6f403279d333b1b7c
Thomas Deinhamer <code@thasmo.dev>
2025-08-06 21:20:12 +0200

    remove various files

--- functions/[[path]].ts
+++
@@ -1,6 +0,0 @@
-import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
-import * as build from '../build/server';
-
-export const onRequest: (context: EventContext<never, never, never>) => Promise<Response> = createPagesFunctionHandler({
-	build,
-} as never);
